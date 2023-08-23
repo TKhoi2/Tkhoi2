d75f1ce0604b011aaea68ab727ec93fa8c4ad90b
TKhoi2 <khoicoi2015@gmail.com>
2023-08-23 19:06:25 +0700

    up

--- Test/resources/views/edit-post.blade.php
+++ Test/resources/views/edit-post.blade.php
@@ -3,9 +3,70 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  <STYle>
+    body, ul {
+  margin: 0;
+  padding: 0;
+}
+
+/* Style for the navbar */
+.navbar {
+  background-color: #333;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+}
+
+/* Style for the logo */
+.logo {
+  font-size: 24px;
+  
+}
+
+/* Style for the logo link */
+.logo a {
+    text-decoration: none;
+    color: #fff;
+    transition: color 0.3s;
+}
+
+.logo a:hover {
+    color: #00ffea;
+}
+
+/* Style for the navigation links */
+.nav-links {
+  list-style: none;
+  display: flex;
+}
+
+.nav-links li {
+  margin-right: 20px;
+}
+
+.nav-links a {
+  text-decoration: none;
+  color: #fff;
+  transition: color 0.3s;
+}
+
+.nav-links a:hover {
+  color: #ff8c00;
+}
+
+  </STYle>
+  <title>EditPost</title>
 </head>
 <body>
+  <nav class="navbar">
+    <div class="logo"><a href="/">RealBook</a></div>
+    <ul class="nav-links">
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
   <h1>Edit Post</h1>
   <form action="/edit-post/{{$post->id}}" method="POST">
     @csrf
--- Test/resources/views/login.blade.php
+++ Test/resources/views/login.blade.php
@@ -4,24 +4,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <Style>
+                      body, ul {
+                    margin: 0;
+                    padding: 0;
+                }
+
+                /* Style for the navbar */
+                .navbar {
+                    background-color: #333;
+                    color: #fff;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+                    padding: 10px 20px;
+                }
+
+                /* Style for the logo */
+                .logo {
+                  font-size: 24px;
+                  
+                }
+
+                /* Style for the logo link */
+                .logo a {
+                    text-decoration: none;
+                    color: #fff;
+                    transition: color 0.3s;
+                }
+
+                .logo a:hover {
+                    color: #00ffea;
+                }
+
+
+                /* Style for the navigation links */
+                .nav-links {
+                    list-style: none;
+                    display: flex;
+                }
+
+                .nav-links li {
+                    margin-right: 20px;
+                }
+
+                .nav-links a {
+                    text-decoration: none;
+                    color: #fff;
+                    transition: color 0.3s;
+                }
+
+                .nav-links a:hover {
+                    color: #ff8c00;
+                }
+    </Style>
     <title>Home</title>
 </head>
 <body>
     @auth
+    <nav class="navbar">
+      <div class="logo"><a href="/">RealBook</a></div>
+      <ul class="nav-links">
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+      </ul>
+  </nav>
     <p>Chúc mừng bạn đã đăng nhập</p>
     
     <div style="border: 3px solid black;">
-        <h2>Create a New Post</h2>
+        <h2>Tạo một Post mới</h2>
         <form action="/create-post" method="POST">
           @csrf
           <input type="text" name="title" placeholder="post title">
           <textarea name="body" placeholder="body content..."></textarea>
-          <button>Save Post</button>
+          <button>Lưu Post</button>
         </form>
       </div>
     
       <div style="border: 3px solid black;">
-        <h2>All Posts</h2>
+        <h2>cách post của bạn</h2>
         @foreach($posts as $post)
         <div style="background-color: gray; padding: 10px; margin: 10px;">
           <h3>{{$post['title']}} bởi {{$post->user->name}}</h3>
